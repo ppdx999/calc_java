e524824ee3d6baef8a1d4a25c164f98a30086b15
fujis <ppdx999@gmail.com>
2025-03-16 22:09:32 +0900

    fixup error when read input from stdin

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -7,15 +7,31 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        try (var scanner = new Scanner(System.in)) {
-            var input = scanner.tokens().toString();
+        try (var stdin = new Scanner(System.in)) {
+            var input = readAll(stdin);
             System.out.println(expr(input));
         }
     };
 
-    public static int expr(String input) {
+    private static StringBuffer readAll(Scanner scanner){
+        var sb = new StringBuffer();
+        while (scanner.hasNext()) {
+            sb.append(scanner.next());
+        }
+        return sb;
+    }
+
+    private static int _expr(String input) {
         var parser = new Parser(input);
         ASTNode ast = parser.parse();
         return ast.evaluate();
+    }
+
+    public static int expr(StringBuffer input) {
+        return _expr(input.toString());
+    }
+
+    public static int expr(String input) {
+        return _expr(input);
     }
 }
